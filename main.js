a49406f049bc7fb577f8cd0ef3f7451d71a87ef6
Afnan Malik <Afnanmalik80@gmail.com>
2022-01-15 19:49:12 +0300

    animation added

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const navbar = document.querySelector(".navigation")
 const hero = document.querySelector(".hero")
 const back_to_top = document.querySelector(".back-to-top")
 
+const sliders = document.querySelectorAll(".slide-up")
+
 // Show navbar and back-to-top button on scroll
 scrollObserverOptions = {
   threshold: 0.5,
@@ -26,3 +28,26 @@ const scrollObserver = new IntersectionObserver(function (
 scrollObserverOptions)
 
 scrollObserver.observe(hero)
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -100px 0px",
+}
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return
+    } else {
+      entry.target.classList.add("appear")
+      appearOnScroll.unobserve(entry.target)
+    }
+  })
+},
+appearOptions)
+
+sliders.forEach((slider) => {
+  appearOnScroll.observe(slider)
+})
